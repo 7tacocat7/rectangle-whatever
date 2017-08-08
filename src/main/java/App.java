57521c395d562ae58e444936c8675ce4585b1fc9
Guest User <Guest@Epicodus-5C.local>
2017-08-08 13:32:30 -0700

    dkdk

---
+++ src/main/java/App.java
@@ -0,0 +1,24 @@
+import models.Rectangle;
+import spark.ModelAndView;
+import spark.template.handlebars.HandlebarsTemplateEngine;
+import static spark.Spark.*;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Map;
+
+public class App {
+    public static void main(String[] args) {
+
+        get("/", (req, res) -> {
+            //just for testing - make two new objects so we have something to retrieve
+            Rectangle rectangle = new Rectangle(3,2);
+            Rectangle otherRectangle = new Rectangle(12, 12);
+
+            Map<String, ArrayList<Rectangle>> model = new HashMap<>();
+            ArrayList myRectangleArrayList = Rectangle.getAll();
+            model.put("myRectangles", myRectangleArrayList);
+            return new ModelAndView(model, "index.hbs");
+        }, new HandlebarsTemplateEngine());
+    }
+}
